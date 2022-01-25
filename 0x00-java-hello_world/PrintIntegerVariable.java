139425a1f0847a40804052c25e4c55d8c85a2b80
flaviagdealmeida <eufga1@gmail.com>
2022-01-25 11:16:45 -0800

    Tasks Java

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -2,6 +2,6 @@ public class PrintIntegerVariable
 {
 	public static void main(String[] args) {
         	int numero = 52;
-        	System.out.println("Avenida Andaló " + numero );
+        	System.out.println("Avenida Andaló, " + numero );
 	}
 }
--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,9 +1,6 @@
 public class PrintStringVariable{
     public static void main(String[] args) {
         String texto = "Holberton School";
-	System.out.println(texto);
-	System.out.println(texto);
-	System.out.println(texto);
-	System.out.println(texto.substring(0,8));
- 	}
-}
+	System.out.println(texto + "\n" + texto +"\n");
+	System.out.println(texto.substring(0,9));
+ 	}}
