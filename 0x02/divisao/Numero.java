40e7bf05c6f4d01e98c0e86d81683d7ca022814c
flaviagdealmeida <eufga1@gmail.com>
2022-01-26 11:39:42 -0800

    Tasks 0 e 1

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,13 +1,13 @@
 public class Numero {
 
 	public static void dividir(int a, int b) {
+		int calculo = 0;
 		try {
-			int calculo = (a/b);
-
-		} catch (Exception e) {
-			e.printStackTrace();
+			calculo = (a/b);
+		} catch (ArithmeticException ex) {
+			System.out.println("Nao eh possivel dividir por zero");
 		} finally {
-			System.out.println("Calculo finalizado");
+			System.out.println(a + " / " +b + " = " + calculo);
 		}
 	}
 
