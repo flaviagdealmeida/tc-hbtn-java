b4bcbfeb3584240a4dd0fc4767a99666362c1625
flaviagdealmeida <eufga1@gmail.com>
2022-01-27 09:56:27 -0800

    Tasks Classes e Objetos

--- 0x03/consistencia/Program.java
+++ 0x03/consistencia/Program.java
@@ -1,4 +1,4 @@
-public class Program {  
+public class Program {
     public static void main(String[] args) {
         PersonagemGame personagem1 = new PersonagemGame(50, "Sonic");
 
@@ -9,7 +9,7 @@ public class Program {
 
         personagem1.receberCura(91);
         personagem1.setNome(null);
-
+        
         System.out.printf("%s, %d, %s\n", personagem1.getNome(), personagem1.getSaudeAtual()
             , personagem1.getStatus());
 
