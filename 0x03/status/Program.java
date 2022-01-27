b0619d906eafcb35d83c4ba881cfbd3e07c62205
flaviagdealmeida <eufga1@gmail.com>
2022-01-27 08:56:02 -0800

    Task Classes e Objetos

--- 0x03/status/Program.java
+++ 0x03/status/Program.java
@@ -1,26 +1,26 @@
-personagem1.setNome("Sonic");
-		
-		personagem1.setSaudeAtual(0);
-	
-		System.out.printf("%d - %s  \n", personagem1.getSaudeAtual(), personagem1.getStatus());
-		
-		personagem1.setSaudeAtual(10);
+public class Program {  
+    public static void main(String[] args) {  
+      PersonagemGame personagem1 = new PersonagemGame();
 
-		System.out.printf("%d - %s  \n", personagem1.getSaudeAtual(), personagem1.getStatus());
+      personagem1.setNome("Sonic");
+      personagem1.setSaudeAtual(0);
 
-		personagem1.receberCura(80);
+      System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
 
-		System.out.printf("%d - %s \n", personagem1.getSaudeAtual(), personagem1.getStatus());
+      personagem1.receberCura(90);
 
-		personagem1.tomarDano(25);
+      System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
 
-		System.out.printf("%d - %s \n", personagem1.getSaudeAtual(), personagem1.getStatus());
+      personagem1.tomarDano(25);
 
-		personagem1.tomarDano(65);
+      System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
 
-		System.out.printf("%d - %s \n", personagem1.getSaudeAtual(), personagem1.getStatus());
-		
-		personagem1.receberCura(10);
-	
-		System.out.printf("%d - %s \n", personagem1.getSaudeAtual(), personagem1.getStatus());
+      personagem1.tomarDano(80);
 
+      System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+
+      personagem1.receberCura(10);
+
+      System.out.printf("%d - %s\n", personagem1.getSaudeAtual(), personagem1.getStatus());
+    }
+ }
