7a56b78297b7b0a6af6d03a0c2264734cee9b836
Flavia Goncalves de Almeida <flaviagoncalves@ciandt.com>
2022-02-01 11:05:15 -0300

    Task conta bancaria

--- 0x05/conta_basica/Program.java
+++ 0x05/conta_basica/Program.java
@@ -1,4 +1,6 @@
 import java.util.Locale;
+import exceptions.OperacaoInvalidaException;
+
 public class Program {
     public static void main(String[] args) {
     	Locale.setDefault(new Locale("pt", "BR"));
