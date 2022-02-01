e2f6a741c9963846edb865b24a1378f07a63aa5e
Flavia Goncalves de Almeida <flaviagoncalves@ciandt.com>
2022-02-01 10:39:40 -0300

    Task Conta Bancaria

---
+++ 0x05/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,8 @@
+public class OperacaoInvalidaException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public OperacaoInvalidaException (String msg) {
+		super (msg);
+	}
+}
