5944a00ae64c67d5ce0e3aa8d68311155c4b790e
Flavia Goncalves de Almeida <flaviagoncalves@ciandt.com>
2022-02-09 17:58:31 -0300

    Alteração de pacote

---
+++ 0x13/resumo_pedido/ProcessadorPedido.java
@@ -0,0 +1,21 @@
+import provedores.ProvedorFrete;
+
+public class ProcessadorPedido {
+
+	private ProvedorFrete provedorFrete;
+
+	
+	public ProcessadorPedido() {
+		
+	}
+
+	public ProcessadorPedido(ProvedorFrete provedorFrete) {
+		super();
+		this.provedorFrete = provedorFrete;
+	}
+	
+	public void processar(Pedido pedido) {
+		Frete calculo = provedorFrete.calcularFrete(pedido.getPeso(), pedido.getTotal());
+		pedido.setFrete(calculo);
+	}
+}
