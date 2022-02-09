7b1486c158b905a0595bd06c0c227a786f4b3335
Flavia Goncalves de Almeida <flaviagoncalves@ciandt.com>
2022-02-09 18:05:57 -0300

    Alteração de pacote

--- 0x13/resumo_pedido/provedores/ProvedorFrete.java
+++
@@ -1,7 +0,0 @@
-package provedores;
-import resumo_pedido.Frete;
-public interface ProvedorFrete {
-
-	public Frete calcularFrete(double peso, double valor);
-	public TipoProvedorFrete obterTipoProvedorFrete();
-}
