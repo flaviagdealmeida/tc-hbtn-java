908af096748806095a5ae2a455fbbefab4f68359
Flávia Almeida <eufga1@gmail.com>
2022-02-17 15:34:17 -0300

    Update ConsultaProdutos.java

--- 0x17/consulta_produtos/ConsultaProdutos.java
+++ 0x17/consulta_produtos/ConsultaProdutos.java
@@ -3,16 +3,8 @@ import java.util.List;
 
 public class ConsultaProdutos {
 	
-	public static List<Produto> filtrar (List<Produto> produtos, CriterioFiltro filtro){
-		 List<Produto> produtoFiltrado = new ArrayList<>();
-		
-		 produtos.forEach(produto -> {
-			 if(filtro.testar(produto)) {
-				 produtoFiltrado.add(produto);
-			 }
-		 });
-		 
-		return produtoFiltrado;
+	public static List<Produto> filtrar (List<Produto> produtos, CriterioFiltro criterioFiltro){
+		return produtos.stream().filter(p -> criterioFiltro.testar(p)).collect(Collectors.toList());
 	}
 }
 
