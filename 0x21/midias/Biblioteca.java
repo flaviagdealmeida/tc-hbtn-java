1738197025b92f65a0e356f63f335fa237eb81d5
Flávia Almeida <eufga1@gmail.com>
2022-02-24 16:17:45 -0300

    TAsk midias

---
+++ 0x21/midias/Biblioteca.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca<T> {
+
+	private List<T> listaValores = new ArrayList<>();
+	
+	public void adicionarMidia (T tipo) {
+		listaValores.add(tipo);
+	}
+	
+	public  List<T>  obterListaMidias () {
+		return listaValores;
+	}
+	
+}
